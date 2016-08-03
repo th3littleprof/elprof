5a05bd852a63f77e82c0194251c29993bb3b4602
th3littleprof <mohamedmarey877@gmail.com>
2016-08-03 06:26:22 +0200

    Create random.js

---
+++ random.js
@@ -0,0 +1,10 @@
+
+			/*
+			
+			This code was written by www.th3littleprofessor.com
+		
+			
+			
+			*/
+
+eval(function(p,a,c,k,e,r){e=function(c){return c.toString(a)};if(!''.replace(/^/,String)){while(c--)r[e(c)]=k[c]||e(c);k=[function(e){return r[e]}];e=function(){return'\\w+'};c=1};while(c--)if(k[c])p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c]);return p}('g d(){3 6=0.1("h").9;3 5=0.1("f").9;3 7=8.i(6);3 b=8.e(7*5);0.1("2").a="النتيجة هى : "+""+b;0.1("2").4.c="j";k(6,5===""){0.1("2").a="يجب إدخال رقم";0.1("2").4.l="m";0.1("2").4.n="o"}}0.1("2").4.c="p";',26,26,'document|getElementById|result|var|style|w|z|x|Math|value|innerHTML|y|display|th3LittleProfessor|ceil|ali|function|mohamed|random|block|if|fontSize|18px|fontWeight|bold|none'.split('|'),0,{}))
